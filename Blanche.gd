f55481839a1bbdfe74133d94ee988b8502c1ef49
Ryan Arana <ryan.arana@hey.com>
2021-02-27 12:47:12 -0800

    Animate the ghost

--- Blanche.gd
+++ Blanche.gd
@@ -2,11 +2,28 @@ extends KinematicBody2D
 
 export var speed := 30
 
-func _physics_process(_delta):
+onready var animationPlayer := $AnimationPlayer
+
+func _physics_process(_delta: float) -> void:
 	var velocity = Vector2.ZERO
 	velocity.y -= Input.get_action_strength("ui_up") * speed
 	velocity.y += Input.get_action_strength("ui_down") * speed
 	velocity.x -= Input.get_action_strength("ui_left") * speed
 	velocity.x += Input.get_action_strength("ui_right") * speed
-	
+
 	move_and_slide(velocity)
+
+func _process(_delta: float) -> void:
+	var moving = false
+	if Input.is_action_pressed("ui_left"):
+		animationPlayer.play("Left")
+		moving = true
+	if Input.is_action_pressed("ui_right"):
+		animationPlayer.play("Right")
+		moving = true
+	if Input.is_action_pressed("ui_up"):
+		animationPlayer.play("Up")
+		moving = true
+
+	if Input.is_action_pressed("ui_down") or not moving:
+		animationPlayer.play("Down")
--- World.gd
+++ World.gd
@@ -8,10 +8,10 @@ onready var light := $Blanche/Light2D
 export var lights_on := false setget turn_lights_on
 
 func _ready():
-	# The lights_on export var is really only used to keep the lights on while 
+	# The lights_on export var is really only used to keep the lights on while
 	# in the editor. This makes sure we start the game with the lights off.
 	self.lights_on = false
-	
+
 func _unhandled_key_input(event):
 	if event.is_action_pressed("Lights"):
 		self.lights_on = !lights_on
