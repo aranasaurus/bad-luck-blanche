52b243e25439c6299e08e8851dab4f4d600b4b2b
Ryan Arana <ryan.arana@hey.com>
2021-02-27 20:58:26 -0800

    Keybind adjustments

--- Blanche.gd
+++ Blanche.gd
@@ -8,30 +8,30 @@ onready var thoughtTimer := $ThoughtTimer
 
 func _physics_process(_delta: float) -> void:
 	var velocity = Vector2.ZERO
-	velocity.y -= Input.get_action_strength("ui_up") * speed
-	velocity.y += Input.get_action_strength("ui_down") * speed
-	velocity.x -= Input.get_action_strength("ui_left") * speed
-	velocity.x += Input.get_action_strength("ui_right") * speed
+	velocity.y -= Input.get_action_strength("Up") * speed
+	velocity.y += Input.get_action_strength("Down") * speed
+	velocity.x -= Input.get_action_strength("Left") * speed
+	velocity.x += Input.get_action_strength("Right") * speed
 
 	move_and_slide(velocity)
 
 func _process(_delta: float) -> void:
 	var moving = false
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("Left"):
 		animationPlayer.play("Left")
 		moving = true
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("Right"):
 		animationPlayer.play("Right")
 		moving = true
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("Up"):
 		animationPlayer.play("Up")
 		moving = true
 
-	if Input.is_action_pressed("ui_down") or not moving:
+	if Input.is_action_pressed("Down") or not moving:
 		animationPlayer.play("Down")
 
 func _unhandled_key_input(event: InputEventKey) -> void:
-	if event.is_action("Say"):
+	if event.is_action("Help"):
 		bubble.show()
 		bubble.text = "Sure is dark in here."
 
